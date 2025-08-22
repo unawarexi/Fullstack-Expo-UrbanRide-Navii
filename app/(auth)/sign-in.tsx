1775d5bd6ff32868841a65d58d2544579e57336e
unawarexi <unawarexi@gmail.com>
2025-08-23 00:33:27 +0100

    auth screens and onbaording

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const SignIn = () => {
+  return (
+    <View>
+      <Text>sign-in</Text>
+    </View>
+  )
+}
+
+export default SignIn
+
+const styles = StyleSheet.create({})
