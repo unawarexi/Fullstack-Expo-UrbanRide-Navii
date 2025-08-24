2cc404ea0352a9d45f92933675c5b6e5cf92a642
unawarexi <unawarexi@gmail.com>
2025-08-24 21:44:08 +0100

    authentication screens znd onboarding

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -2,10 +2,10 @@ import { Stack } from "expo-router";
 
 const Layout = () => {
   return (
-    <Stack>
-      <Stack.Screen name="onboarding" options={{ headerShown: false }} />
-      <Stack.Screen name="sign-up" options={{ headerShown: false }} />
-      <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+    <Stack screenOptions={{ headerShown: false}}>
+      <Stack.Screen name="Onboarding"  />
+      <Stack.Screen name="sign-up"  />
+      <Stack.Screen name="sign-in"  />
     </Stack>
   );
 };
