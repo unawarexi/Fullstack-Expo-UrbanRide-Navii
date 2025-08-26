a1acd815681ccf4a0530b00198c2aeca5625533f
unawarexi <unawarexi@gmail.com>
2025-08-27 00:21:46 +0100

    formatters and helperfunction utilities

---
+++ core/utils/HelperFunctions.ts
@@ -0,0 +1,11 @@
+import { Ride } from "@/core/types/type";
+
+export const sortRides = (rides: Ride[]): Ride[] => {
+  const result = rides.sort((a, b) => {
+    const dateA = new Date(`${a.created_at}T${a.ride_time}`);
+    const dateB = new Date(`${b.created_at}T${b.ride_time}`);
+    return dateB.getTime() - dateA.getTime();
+  });
+
+  return result.reverse();
+};
