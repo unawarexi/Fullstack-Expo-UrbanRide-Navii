4e6580bc14d13a28b296f88a1f8945e420b70cae
unawarexi <unawarexi@gmail.com>
2025-09-01 22:09:22 +0100

    zustand driver store and location store states

--- global/use.location.store.ts
+++ global/use.location.store.ts
@@ -1,6 +1,38 @@
-import { LocationStore } from "@/core/types/type";
+import { LocationData, LocationStore } from "@/core/types/type";
 import { create } from "zustand";
 
 export const useLocationStore = create<LocationStore>((set) => ({
-
-})
+  userLatitude: null,
+  userLongitude: null,
+  userAddress: null,
+  destinationLatitude: null,
+  destinationLongitude: null,
+  destinationAddress: null,
+  userLocationData: null,
+  destinationLocationData: null,
+  setUserLocation: ({ latitude, longitude, address, locationData }: { latitude: number; longitude: number; address: string; locationData?: LocationData }) =>
+    set((state) => ({
+      userLatitude: latitude,
+      userLongitude: longitude,
+      userAddress: address,
+      userLocationData: locationData || state.userLocationData,
+    })),
+  setDestinationLocation: ({ latitude, longitude, address, locationData }: { latitude: number; longitude: number; address: string; locationData?: LocationData }) =>
+    set((state) => ({
+      destinationLatitude: latitude,
+      destinationLongitude: longitude,
+      destinationAddress: address,
+      destinationLocationData: locationData || state.destinationLocationData,
+    })),
+  clearLocations: () =>
+    set({
+      userLatitude: null,
+      userLongitude: null,
+      userAddress: null,
+      destinationLatitude: null,
+      destinationLongitude: null,
+      destinationAddress: null,
+      userLocationData: null,
+      destinationLocationData: null,
+    }),
+}));
